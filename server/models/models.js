09e3e03a5d380b91998d1cb0871cafd67c635423
Jopkins <sergebrew@gmail.com>
2022-08-19 00:40:48 +0300

    business logic, queries to DB and response

--- server/models/models.js
+++ server/models/models.js
@@ -49,32 +49,32 @@ const TypeBrand = sequelize.define('type_brand', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
 });
 
-User.hasOne(Basket)
-Basket.belongsTo(User)
+User.hasOne(Basket);
+Basket.belongsTo(User);
 
-User.hasMany(Rating)
-Rating.belongsTo(User)
+User.hasMany(Rating);
+Rating.belongsTo(User);
 
-Basket.hasMany(BasketProduct)
-BasketProduct.belongsTo(Basket)
+Basket.hasMany(BasketProduct);
+BasketProduct.belongsTo(Basket);
 
-Type.hasMany(Product)
-Product.belongsTo(Type)
+Type.hasMany(Product);
+Product.belongsTo(Type);
 
-Brand.hasMany(Product)
-Product.belongsTo(Brand)
+Brand.hasMany(Product);
+Product.belongsTo(Brand);
 
-Product.hasMany(Rating)
-Rating.belongsTo(Product)
+Product.hasMany(Rating);
+Rating.belongsTo(Product);
 
-Product.hasMany(BasketProduct)
-BasketProduct.belongsTo(Product)
+Product.hasMany(BasketProduct);
+BasketProduct.belongsTo(Product);
 
-Product.hasMany(ProductInfo)
-ProductInfo.belongsTo(Product)
+Product.hasMany(ProductInfo, {as: 'info'});
+ProductInfo.belongsTo(Product);
 
-Type.belongsToMany(Brand, {through: TypeBrand})
-Brand.belongsToMany(Type, {through: TypeBrand})
+Type.belongsToMany(Brand, {through: TypeBrand});
+Brand.belongsToMany(Type, {through: TypeBrand});
 
 module.exports = {
   User,
